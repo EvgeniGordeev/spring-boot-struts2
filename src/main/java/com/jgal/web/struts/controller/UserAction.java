c356f145106ac729fabf922959dbe46fc7b8299c
Evgeni Gordeev <egordeev@conserviscorp.com>
2018-08-08 12:29:58 -0500

    struts upgraded to 2.5.16

--- src/main/java/com/jgal/web/struts/controller/UserAction.java
+++ src/main/java/com/jgal/web/struts/controller/UserAction.java
@@ -1,15 +1,18 @@
 package com.jgal.web.struts.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import com.jgal.web.struts.model.User;
 import com.jgal.web.struts.service.ServiceFacade;
 import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
+@Namespace("/users")
 public class UserAction extends ActionSupport {
 
     private static final long serialVersionUID = 1L;
@@ -18,7 +21,10 @@ public class UserAction extends ActionSupport {
     @Autowired
     private ServiceFacade userService;
 
-    public String execute() throws Exception {
+    @Action(value = "index", results = {
+        @Result(name = "success", location = "/users.jsp")
+    })
+    public String index() {
         users = userService.retrieveUsers();
         return SUCCESS;
     }
